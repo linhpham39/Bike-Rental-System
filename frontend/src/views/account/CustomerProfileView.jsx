686ec7282f1b7c00547c76604182e1a3ebfd6518
Ngo Quang Minh <quangminhngo3@gmail.com>
2023-08-17 16:30:15 +0700

    Update Profile

--- frontend/src/views/account/CustomerProfileView.jsx
+++ frontend/src/views/account/CustomerProfileView.jsx
@@ -44,25 +44,25 @@ const CustomerProfile = () => {
     mobileNumber,
     email,
     birthday,
-    location,
-    shippingInformation,
-    paymentMethods
+    location
+
+
   } = customer;
 
   return (
     <div className="container">
       <h3 className="mb-4">
-      <br></br>
+        <br></br>
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-vcard" viewBox="0 0 16 16">
           <path d="M5 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm4-2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5ZM9 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4A.5.5 0 0 1 9 8Zm1 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5Z" />
           <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2ZM1 4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H8.96c.026-.163.04-.33.04-.5C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1.006 1.006 0 0 1 1 12V4Z" />
         </svg>   Customer Profile
       </h3>
       <div className="row">
-      <div className="col-md-4">
-    <br />
-    <img src={avatarImageUrl} alt="Avatar" className="avatar-img img-fluid rounded fit-image" />
-</div>
+        <div className="col-md-4">
+          <br />
+          <img src={avatarImageUrl} alt="Avatar" className="avatar-img img-fluid rounded fit-image" />
+        </div>
 
         <div className="col-md-8">
           <form>
@@ -85,7 +85,7 @@ const CustomerProfile = () => {
                     <i className="bi bi-calendar3"></i>
                   </span>
                 </div>
-                <input type="text" value={birthday} className="form-control" readOnly />
+                <input type="text" value={birthday.substring(0, 10)} className="form-control" readOnly />
               </div>
             </div>
             <div className="form-group">
@@ -140,9 +140,9 @@ const CustomerProfile = () => {
               </Link>
               {isAdmin && (
                 <Link to="/admin" state={customer}>
-                <button type="button" className="admin-panel-btn btn btn-danger">
-                  Go to Administrator panel
-                </button>
+                  <button type="button" className="admin-panel-btn btn btn-danger">
+                    Go to Administrator panel
+                  </button>
                 </Link>
               )}
             </div>
@@ -160,7 +160,7 @@ const CustomerProfile = () => {
               <i className="bi bi-house-fill"></i>
             </span>
           </div>
-          <input type="text" value={location.address} className="form-control" readOnly />
+          <input type="text" value={location && location.address} className="form-control" readOnly />
         </div>
       </div>
       <div className="form-group">
@@ -171,7 +171,7 @@ const CustomerProfile = () => {
               <i className="bi bi-geo-fill"></i>
             </span>
           </div>
-          <input type="text" value={location.district} className="form-control" readOnly />
+          <input type="text" value={location && location.district} className="form-control" readOnly />
         </div>
       </div>
       <div className="form-group">
@@ -184,66 +184,10 @@ const CustomerProfile = () => {
               </svg>
             </span>
           </div>
-          <input type="text" value={location.city} className="form-control" readOnly />
+          <input type="text" value={location && location.city} className="form-control" readOnly />
         </div>
       </div>
-      {!isAdmin && <h3 className="mt-4 mb-4">
-        <i className="bi bi-truck"></i> Shipping Information
-      </h3>}
-      {!isAdmin && <div className="section">
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="text-center">Shipping</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Address</th>
-              <th>District</th>
-              <th>City</th>
-            </tr>
-          </thead>
-          <tbody>
-            {shippingInformation.map((info, index) => (
-              <tr key={index}>
-                <td className="text-center">{index + 1}</td>
-                <td>{info.firstName}</td>
-                <td>{info.lastName}</td>
-                <td>{info.address}</td>
-                <td>{info.district}</td>
-                <td>{info.city}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>}
 
-      {!isAdmin && <h3 className="mt-4 mb-4">
-        <i className="bi bi-credit-card"></i> Payment Methods
-      </h3>}
-      {!isAdmin && <div className="section">
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="text-center">Payment Method</th>
-              <th>Card Number</th>
-              <th>Name on Card</th>
-              <th>Expiration Month</th>
-              <th>Expiration Year</th>
-            </tr>
-          </thead>
-          <tbody>
-            {paymentMethods.map((method, index) => (
-              <tr key={index}>
-                <td className="text-center">{index + 1}</td>
-                <td>{method.cardNumber}</td>
-                <td>{method.nameOnCard}</td>
-                <td>{method.expirationMonth}</td>
-                <td>{method.expirationYear}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>}
 
     </div>
   );
