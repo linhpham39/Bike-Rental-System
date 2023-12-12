2b0827299af39ca4eb9876f9daab74ef6477f925
linhpham39 <linhshark02@gmail.com>
2023-12-12 11:31:36 +0700

    test ci/cd 2

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const mongoose = require('./config/mongoose');
 
 const PORT = process.env.PORT || 3000;
 
-// cmt for testing CI/CD, test tiep, fail hoai :((
+// cmt for testing CI/CD, test tiep, fail hoai :((, dm final version
 mongoose.start();
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
